5230feca416698f32c5ca585aee79faf7f3f8444
lindomash001 <lindomash001@gmail.com>
2025-03-03 05:18:00 +0200

    added const file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:glyph_notes/const/const.dart';
+import 'package:glyph_notes/model/note_model.dart';
 import 'package:glyph_notes/pages/main_page.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  
+  Hive.registerAdapter(NoteAdapter());
+  Hive.registerAdapter(FolderAdapter());
+  
+  await Hive.openBox<Note>(noteBox);
+  await Hive.openBox<Folder>(foldersBox);
   runApp(const MyApp());
 }
 
